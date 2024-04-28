0e6dc2138ea51d230a636b95d7eb3e084476a4b3
Michael <92275614+m1ch4el0@users.noreply.github.com>
2024-04-28 17:02:34 +0200

    [Fix] add ISDB download

--- docker/scripts/download_db.sh
+++ docker/scripts/download_db.sh
@@ -6,3 +6,4 @@ wget https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase
 gunzip uniprot_sprot.fasta.gz
 wget https://marks.hms.harvard.edu/evcomplex_databases/cds_pro_2017_02.txt
 wget https://marks.hms.harvard.edu/evcomplex_databases/idmapping_uniprot_embl_2017_02.txt 
+wget https://github.com/KohlbacherLab/ISDB/blob/main/db_2024_04_28.db
